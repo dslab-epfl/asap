ae6bfb346fefbce2f6afa55477b065c9f397d94f
Jonas Wagner <jonas.wagner@epfl.ch>
2015-11-16 12:25:27 +0100

    Update .ycm_extra_conf.py to use CMake's compilation database.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,25 +1,27 @@
 import os
 import ycm_core
 
+# These are the compilation flags that will be used in case there's no
+# compilation database set (by default, one is not set).
+# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-I../llvm-build/include',
+'-Wall',
+# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
+# language to use when compiling headers. So it will guess. Badly. So C++
+# headers will be compiled as C headers. You don't want that so ALWAYS specify
+# a "-std=<something>".
+# For a C project, you would set this to something like 'c99' instead of
+# 'c++11'.
+'-std=c++11',
+# ...and the same thing goes for the magic -x option which specifies the
+# language that the files to be compiled are written in. This is mostly
+# relevant for c++ headers.
+# For a C project, you would set this to 'c' instead of 'c++'.
+'-x',
+'c++',
+'-I../build/include',
 '-Iinclude',
 '-Iprojects/compiler-rt/lib/',
-'-W',
-'-Wall',
-'-Wcovered-switch-default',
-'-Wmissing-field-initializers',
-'-Wno-long-long',
-'-Wno-unused-parameter',
-'-Wnon-virtual-dtor',
-'-Wwrite-strings',
-'-fPIC',
-'-fno-exceptions',
-'-fno-rtti',
-'-fvisibility-inlines-hidden',
-'-pedantic',
-'-std=c++11',
-'-x', 'c++'
 ]
 
 
@@ -33,7 +35,9 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = ''
+compilation_database_folder = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath( __file__ ))),
+        'build')
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
